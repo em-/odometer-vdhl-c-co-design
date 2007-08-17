b953b2d988b2b3e964c22664fe47a70f3de021ed
Emanuele Aina <em@nerd.ocracy.org>
2007-08-17 11:50:16 +0200

    Make encoder signal generation more automatic

--- hardware/encoder.vhdl
+++ hardware/encoder.vhdl
@@ -15,26 +15,70 @@ entity encoder is
 end encoder;
 
 architecture behavioral of encoder is
+   type STATE is (S_00, S_10, S_11, S_01);
+   signal current_state: STATE;
+   signal left, right: boolean;
+   signal tick: boolean; -- used only to generate events
 begin
+
 process
 begin
-    A <= '0';
-    B <= '0';
+    left <= true;
+    tick <= true;
     wait for 1 us;
-    A <= '1';
+    tick <= not tick;
     wait for 5 us;
-    B <= '1';
+    tick <= not tick;
     wait for 2 us;
-    A <= '0';
+    tick <= not tick;
     wait for 5 us;
-    B <= '0';
+    tick <= not tick;
     report "foo";
     wait for 2 us;
-    A <= '1';
+    tick <= not tick;
     wait for 5 us;
-    B <= '1';
+    tick <= not tick;
     wait for 2 us;
-    A <= '0';
+    tick <= not tick;
     wait;
 end process;
+
+process(tick)
+begin
+   case current_state is
+   when S_00 =>
+      if    left  then current_state <= S_10;
+      elsif right then current_state <= S_01;
+      end if;
+
+   when S_10 =>
+      if    left  then current_state <= S_11;
+      elsif right then current_state <= S_00;
+      end if;
+
+   when S_11 =>
+      if    left  then current_state <= S_01;
+      elsif right then current_state <= S_10;
+      end if;
+
+   when S_01 =>
+      if    left  then current_state <= S_00;
+      elsif right then current_state <= S_11;
+      end if;
+
+   when others =>
+       current_state <= S_00;
+   end case;
+end process;
+
+process(current_state)
+begin
+   case current_state is
+   when S_00 => A<='0'; B<='0'; Z<='1';
+   when S_01 => A<='0'; B<='1'; Z<='0';
+   when S_11 => A<='1'; B<='1'; Z<='0';
+   when S_10 => A<='1'; B<='0'; Z<='0';
+   end case;
+end process;
+
 end behavioral;
