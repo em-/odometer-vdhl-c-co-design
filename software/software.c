4509251de162e80097badfa9d236fe74d5feae6f
Emanuele Aina <em@nerd.ocracy.org>
2007-08-19 18:19:53 +0200

    Reworked the serial input command handling

--- software/software.c
+++ software/software.c
@@ -15,26 +15,41 @@
 #define SERIAL_STATUS_RXAV   0x1
 #define SERIAL_STATUS_TXBUSY 0x2
 
-enum _serial_wait {
-    SERIAL_WAIT_COMMAND,
-    SERIAL_WAIT_DATA_1,
-    SERIAL_WAIT_DATA_2
-};
-typedef enum _serial_wait serial_wait;
+#define COMMANDS 4
 
-enum _command {
-    COMMAND_SET_COEFF       = 0,
-    COMMAND_SET_K           = 1,
-    COMMAND_SET_K1          = 2,
-    COMMAND_SET_K2          = 3,
-    COMMAND_GET_ANGLE       = 4,
-    COMMAND_GET_REVOLUTIONS = 5
-};
-typedef enum _command command;
+typedef struct {
+    int data_size;
+    void (*handler)(int data);
+} handler;
 
-void command_received(int command, int data) {
-    fprintf(stderr, "received command %d: %d\n", command, data);
+int coeff, K, K1, K2, angle, revolutions;
+
+void set_coeff(int data) {
+    coeff = data;
+    fprintf(stderr, "setting coeff: %d\n", coeff);
 }
+
+void set_K(int data) {
+    K = data;
+    fprintf(stderr, "setting K: %d\n", K);
+}
+
+void set_K1(int data) {
+    K1 = data;
+    fprintf(stderr, "setting K1: %d\n", K1);
+}
+
+void set_K2(int data) {
+    K2 = data;
+    fprintf(stderr, "setting K2: %d\n", K2);
+}
+
+handler command_handlers[COMMANDS] = {
+    {2, set_coeff},
+    {2, set_K},
+    {2, set_K1},
+    {2, set_K2},
+};
 
 void encoder_left(void)
 {
@@ -53,36 +68,23 @@ void encoder_revolution(void)
 
 void serial_received_data(unsigned char data)
 {
-    static serial_wait wait = SERIAL_WAIT_COMMAND;
-    static unsigned char command, data_1;
-    int command_data;
+    static int bytes_left = 0;
+    static int command, command_data;
 
-    switch(wait) {
-        case SERIAL_WAIT_COMMAND:
-            command = data;
-            switch(command) {
-                case COMMAND_SET_COEFF:
-                case COMMAND_SET_K:
-                case COMMAND_SET_K1:
-                case COMMAND_SET_K2:
-                    wait = SERIAL_WAIT_DATA_1;
-                    break;
-                default:
-                    wait = SERIAL_WAIT_COMMAND;
-                    command_received(command, 0);
-            }
-            break;
-        case SERIAL_WAIT_DATA_1:
-            data_1 = data;
-            wait = SERIAL_WAIT_DATA_2;
-            break;
-        case SERIAL_WAIT_DATA_2:
-            command_data = data_1;
-            command_data = command_data << 8;
-            command_data = command_data | data;
-            wait = SERIAL_WAIT_COMMAND;
-            command_received(command, command_data);
-            break;
+    if (bytes_left == 0) {
+        command = data;
+        command_data = 0;
+        if (command < COMMANDS) {
+            bytes_left = command_handlers[command].data_size;
+        }
+    } else {
+        command_data = command_data << 8;
+        command_data = command_data | data;
+        bytes_left--;
+    }
+
+    if (bytes_left == 0 && command < COMMANDS) {
+        command_handlers[command].handler(command_data);
     }
 }
 
