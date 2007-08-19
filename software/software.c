98033e3297971e2f5040c44826d8042154720afe
Emanuele Aina <em@nerd.ocracy.org>
2007-08-19 16:37:49 +0200

    Accumulate commands and data from the serial input

--- software/software.c
+++ software/software.c
@@ -15,6 +15,13 @@
 #define SERIAL_STATUS_RXAV   0x1
 #define SERIAL_STATUS_TXBUSY 0x2
 
+enum _serial_wait {
+    SERIAL_WAIT_COMMAND,
+    SERIAL_WAIT_DATA_1,
+    SERIAL_WAIT_DATA_2
+};
+typedef enum _serial_wait serial_wait;
+
 void encoder_left(void)
 {
     fprintf(stderr, "tick left\n"); 
@@ -32,7 +39,24 @@ void encoder_revolution(void)
 
 void serial_received_data(data)
 {
-    fprintf(stderr, "received command %d\n", data);
+    static serial_wait wait = SERIAL_WAIT_COMMAND;
+    static int command, data_1, data_2;
+
+    switch(wait) {
+        case SERIAL_WAIT_COMMAND:
+            command = data;
+            wait = SERIAL_WAIT_DATA_1;
+            break;
+        case SERIAL_WAIT_DATA_1:
+            data_1 = data;
+            wait = SERIAL_WAIT_DATA_2;
+            break;
+        case SERIAL_WAIT_DATA_2:
+            data_2 = data;
+            wait = SERIAL_WAIT_COMMAND;
+            fprintf(stderr, "received command %d: %d %d\n", command, data_1, data_2);
+            break;
+    }
 }
 
 void serial_line_free()
