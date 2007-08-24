ba3ffab632a6c9368b4d31df819ed70af338bf9c
Emanuele Aina <em@nerd.ocracy.org>
2007-08-24 12:58:17 +0200

    Renamed the Odometer related functions to odometer_*

--- software/software.c
+++ software/software.c
@@ -38,7 +38,7 @@ void odometer_init(Odometer *odometer)
     odometer->coeff = odometer->K = odometer->K1 = odometer->K2 =0; 
 }
 
-void check_angle(void)
+void odometer_check_angle(void)
 {
     if ((odometer.angle > odometer.K1) 
         && (odometer.angle < odometer.K2)
@@ -48,57 +48,57 @@ void check_angle(void)
     }
 }
 
-void set_coeff(int data) {
+void odometer_set_coeff(int data) {
     odometer.coeff = data;
     fprintf(stderr, "setting coeff: %d\n", odometer.coeff);
 }
 
-void set_K(int data) {
+void odometer_set_K(int data) {
     odometer.K = data;
     fprintf(stderr, "setting K: %d\n", odometer.K);
 }
 
-void set_K1(int data) {
+void odometer_set_K1(int data) {
     odometer.K1 = data;
     fprintf(stderr, "setting K1: %d\n", odometer.K1);
 }
 
-void set_K2(int data) {
+void odometer_set_K2(int data) {
     odometer.K2 = data;
     fprintf(stderr, "setting K2: %d\n", odometer.K2);
 }
 
-void get_angle(int data) {
+void odometer_get_angle(int data) {
     serial_send(&serial_interface, odometer.angle);
     fprintf(stderr, "getting angle: %d\n", odometer.angle);
 }
 
-void get_revolutions(int data) {
+void odometer_get_revolutions(int data) {
     serial_send(&serial_interface, odometer.revolutions);
     fprintf(stderr, "getting revolutions: %d\n", odometer.revolutions);
 }
 
-void encoder_counterclockwise(void *data)
+void odometer_encoder_counterclockwise(void *data)
 {
     fprintf(stderr, "tick counter-clockwise %d-%d\n", odometer.angle, odometer.coeff);
     if (odometer.started) {
         odometer.angle -= odometer.coeff;
         odometer.direction = DIRECTION_COUNTERCLOCKWISE;
-        check_angle();
+        odometer_check_angle();
     }
 }
 
-void encoder_clockwise(void *data)
+void odometer_encoder_clockwise(void *data)
 {
     fprintf(stderr, "tick clockwise %d+%d\n", odometer.angle, odometer.coeff);
     if (odometer.started) {
         odometer.angle += odometer.coeff;
         odometer.direction = DIRECTION_CLOCKWISE;
-        check_angle();
+        odometer_check_angle();
     }
 }
 
-void encoder_revolution(void *data)
+void odometer_encoder_revolution(void *data)
 {
     if (odometer.coeff != 0)
         odometer.started = 1;
@@ -117,12 +117,12 @@ int main(void)
 {
     int i, command_nr;
     SerialHandler command_handlers[] = {
-        {2, set_coeff},
-        {2, set_K},
-        {2, set_K1},
-        {2, set_K2},
-        {0, get_angle},
-        {0, get_revolutions}
+        {2, odometer_set_coeff},
+        {2, odometer_set_K},
+        {2, odometer_set_K1},
+        {2, odometer_set_K2},
+        {0, odometer_get_angle},
+        {0, odometer_get_revolutions}
     };
 
     serial_init(&serial_interface);
@@ -131,9 +131,9 @@ int main(void)
     command_nr = sizeof(command_handlers)/sizeof(SerialHandler);
     serial_set_command_handlers(&serial_interface, command_handlers, command_nr);
 
-    set_irq_handler(0, encoder_counterclockwise, NULL);
-    set_irq_handler(1, encoder_clockwise, NULL);
-    set_irq_handler(2, encoder_revolution, NULL);
+    set_irq_handler(0, odometer_encoder_counterclockwise, NULL);
+    set_irq_handler(1, odometer_encoder_clockwise, NULL);
+    set_irq_handler(2, odometer_encoder_revolution, NULL);
     set_irq_handler(3, serial_notify, &serial_interface);
 
     for(i=0; i<10000; i++)
