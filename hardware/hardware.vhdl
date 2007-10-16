5beca16430a74499bccd4981231920e935469007
Emanuele Aina <em@nerd.ocracy.org>
2007-10-16 23:17:36 +0200

    Reordered the components in the hardware entity

--- hardware/hardware.vhdl
+++ hardware/hardware.vhdl
@@ -10,6 +10,22 @@ entity hardware is
 end hardware;
 
 architecture behavioral of hardware is
+    component encoder_interface
+        port (CLK, RST: in std_logic;
+              A, B, Z:  in std_logic;
+              COUNTERCLOCKWISE, CLOCKWISE, REVOLUTION: out std_logic);
+    end component;
+
+    component output_interface
+        generic (BASE_ADDR: std_logic_vector(15 downto 0));
+        port (CLK, RST:     in  std_logic;
+              TICK:         out std_logic;
+              BUS_STROBE:   in  std_logic;
+              BUS_RnW:      in  std_logic;
+              BUS_ADDR:     in  std_logic_vector(15 downto 0);
+              BUS_DATA_IN:  in  std_logic_vector(15 downto 0));
+    end component;
+
     component simulator_hwsw_interface
         port(
             CLK, RST: in  std_logic;
@@ -41,32 +57,6 @@ architecture behavioral of hardware is
               FINISH: in boolean);
     end component;
 
-    component encoder_interface
-        port (CLK, RST: in std_logic;
-              A, B, Z:  in std_logic;
-              COUNTERCLOCKWISE, CLOCKWISE, REVOLUTION: out std_logic);
-    end component;
-
-    component output_interface
-        generic (BASE_ADDR: std_logic_vector(15 downto 0));
-        port (CLK, RST:     in  std_logic;
-              TICK:         out std_logic;
-              BUS_STROBE:   in  std_logic;
-              BUS_RnW:      in  std_logic;
-              BUS_ADDR:     in  std_logic_vector(15 downto 0);
-              BUS_DATA_IN:  in  std_logic_vector(15 downto 0));
-    end component;
-
-    component simulator_uart
-        port (CLK, RST:     in  std_logic;
-              RxData:       out std_logic_vector(7 downto 0);
-              TxData:       in  std_logic_vector(7 downto 0);
-              RxAv:         out std_logic;
-              TxBusy:       out std_logic;
-              ReadA:        in  std_logic;
-              LoadA:        in  std_logic);
-    end component;
-
     component simulator_serial_controller
         generic (BASE_ADDR: std_logic_vector(15 downto 0));
         port (CLK, RST:     in  std_logic;
@@ -82,6 +72,16 @@ architecture behavioral of hardware is
               BUS_DATA_IN:  in  std_logic_vector(15 downto 0);
               BUS_DATA_OUT: out std_logic_vector(15 downto 0);
               IRQ_OUT:      out std_logic);
+    end component;
+
+    component simulator_uart
+        port (CLK, RST:     in  std_logic;
+              RxData:       out std_logic_vector(7 downto 0);
+              TxData:       in  std_logic_vector(7 downto 0);
+              RxAv:         out std_logic;
+              TxBusy:       out std_logic;
+              ReadA:        in  std_logic;
+              LoadA:        in  std_logic);
     end component;
 
     signal FINISH: boolean := false;
@@ -123,6 +123,13 @@ begin
     end if;
 end process;
 
+enc_iface: encoder_interface
+    port map(CLK, RST, A, B, Z, REQUESTS(0), REQUESTS(1), REQUESTS(2));
+
+output_iface: output_interface
+    generic map (BASE_ADDR => X"1003")
+    port map(CLK, RST, TICK, BUS_STROBE, BUS_RnW, BUS_ADDR, BUS_DATA_IN);
+
 hwsw: simulator_hwsw_interface
     port map(CLK, RST, BUS_DATA_OUT, IRQ,
              BUS_STROBE, BUS_RnW, BUS_ADDR, BUS_DATA_IN, FINISH);
@@ -135,20 +142,13 @@ irq_ctrl: simulator_interrupt_controller
 enc: simulator_encoder
     port map(A, B, Z, FINISH);
 
-enc_iface: encoder_interface
-    port map(CLK, RST, A, B, Z, REQUESTS(0), REQUESTS(1), REQUESTS(2));
-
-u: simulator_uart
-    port map(CLK, RST, RxData, TxData, RxAv, TxBusy, ReadA, LoadA);
-
 serial_iface: simulator_serial_controller
     generic map (BASE_ADDR => X"1001")
     port map(CLK, RST, RxData, TxData, RxAv, TxBusy, ReadA, LoadA,
              BUS_STROBE, BUS_RnW, BUS_ADDR, BUS_DATA_IN, BUS_DATA_OUT,
              REQUESTS(3));
 
-output_iface: output_interface
-    generic map (BASE_ADDR => X"1003")
-    port map(CLK, RST, TICK, BUS_STROBE, BUS_RnW, BUS_ADDR, BUS_DATA_IN);
+u: simulator_uart
+    port map(CLK, RST, RxData, TxData, RxAv, TxBusy, ReadA, LoadA);
 
 end behavioral;
